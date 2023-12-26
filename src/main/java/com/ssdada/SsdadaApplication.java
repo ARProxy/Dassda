8e15c50f4ceef379d1955e5745b7a068aa469b04
ARProxy <hocci0222@kakao.com>
2023-12-26 23:00:06 +0900

    first commit

---
+++ src/main/java/com/ssdada/SsdadaApplication.java
@@ -0,0 +1,13 @@
+package com.ssdada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsdadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsdadaApplication.class, args);
+	}
+
+}
